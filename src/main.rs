bbbb0bf672998640a5632a602ddab6a23b304e3d
HeroesLament <nunya@business.org>
2024-08-20 22:30:40 -0800

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+fn hello_world() {
+    println!("hello world!");
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Mac Wynkoop".to_string())));
+    commands.spawn((Person, Name("Dalton Nabors".to_string())));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in &query {
+        println!("hello {}!", name.0);
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Mac Wynkoop" {
+            name.0 = "Mac Gigachad".to_string();
+        }
+
+        else if name.0 == "Dalton Nabors" {
+            name.0 = "Dalton Gigachad".to_string();
+        }
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_people)
+        .add_systems(Update, (hello_world, (update_people, greet_people).chain()))
+        .run();
+}
