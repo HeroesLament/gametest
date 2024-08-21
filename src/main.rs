2989c23e83c89fe6483630378b76f4687ee439d4
HeroesLament <nunya@business.org>
2024-08-20 23:51:49 -0800

    Add timer

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ struct Name(String);
 #[derive(Component)]
 struct Idd;
 
+#[derive(Resource)]
+struct GreetTimer(Timer);
+
 pub struct HelloPlugin;
 
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
         app.add_systems(Startup, add_people);
         app.add_systems(Update, (update_people, greet_people).chain());
     }
-}
-
-fn hello_world() {
-    println!("hello world!");
 }
 
 fn add_people(mut commands: Commands) {
@@ -27,9 +27,11 @@ fn add_people(mut commands: Commands) {
     commands.spawn((Person, Name("Dalton Nabors".to_string())));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in &query {
-        println!("hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
     }
 }
 
